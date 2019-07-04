1ab165e434ab159316126441a20a2d72c34de088
Alivelu <alivelu7193@gmail.com>
2019-07-04 12:06:08 +0530

    first commit

--- JSON/ViewController.swift
+++ JSON/ViewController.swift
@@ -10,11 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var URLSessionObj:URLSession?
+    var URLReq:URLRequest?
+    var dataTask:URLSessionDataTask?
+    
+    
+    @IBOutlet weak var TF1: UITextField!
+    @IBOutlet weak var TF2: UITextField!
+    
+  
+
+    @IBAction func btnAction(_ sender: Any) {
+        
+        
+        self.URLSessionObj=URLSession(configuration: .default)
+        self.URLReq=URLRequest(url: URL(string:"https:www.brninfotech.com/pulse/modules/admin/ValidateLogin.php")!)
+        self.URLReq?.httpMethod="POST"
+        
+        
+        
+        
+        let dataToPass="funcName=verifyLogin&registeredEmail="+self.TF1.text!+"&registeredPassword="+self.TF2.text!
+        self.URLReq?.httpBody=dataToPass.data(using: String.Encoding.utf8)
+        self.dataTask=self.URLSessionObj?.dataTask(with: URLReq!, completionHandler: {(data,resp,error) in
+            
+            print(data)
+            
+            do{
+                var serverRespDic:[String:String]=try
+                
+                JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments) as! [String:String]
+                
+                print(serverRespDic)
+            }
+                catch
+                {
+                    print("something went wrong")
+                }
+            })
+        
+        self.dataTask?.resume()
+        
+    }
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
